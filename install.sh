94ac51e17f8e886df22ceb7524c71fe71fb18539
Alexis Tremblay <alexis.tremblay@gmail.com>
2017-12-16 14:05:35 -0500

    Add install script

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+if [ -e $HOME/.vimrc ]; then
+    echo "Moving old \$HOME/.vimrc to \$HOME/.vimrc.old"
+    mv $HOME/.vimrc $HOME/.vimrc.old
+fi
+ln -s $HOME/dotfiles/vim/vimrc $HOME/.vimrc
+
+if [ -e $HOME/.tmux.conf ]; then
+    echo "Moving old \$HOME/.tmux.conf to \$HOME/.tmux.conf.old"
+    mv $HOME/.tmux.conf $HOME/.tmux.conf.old
+fi
+ln -s $HOME/dotfiles/tmux/tmux.conf $HOME/.tmux.conf
+
+if [ -e $HOME/.zshrc ]; then
+    echo "Moving old \$HOME/.zshrc to \$HOME/.zshrc.old"
+    mv $HOME/.zshrc $HOME/.zshrc.old
+fi
+ln -s $HOME/dotfiles/zsh/zshrc $HOME/.zshrc
+
+if [ -e $HOME/.zshaliases ]; then
+    echo "Moving old \$HOME/.zshaliases to \$HOME/.zshaliases.old"
+    mv $HOME/.zshaliases $HOME/.zshaliases.old
+fi
+ln -s $HOME/dotfiles/zsh/zshaliases $HOME/.zshaliases
+
