1d2073ea603722c24f95022dc96253a1acb38c10
Alexis Tremblay <alexis.tremblay@gmail.com>
2021-07-23 09:47:54 -0400

    remove comments

--- nvim/lua/lsp-config.lua
+++ nvim/lua/lsp-config.lua
@@ -49,33 +49,6 @@ local on_attach = function(client, bufnr)
     'ﬦ', -- Operator
     '', -- TypeParameter
   }
-  --protocol.CompletionItemKind = {
-    --'', -- Text
-    --'', -- Method
-    --'', -- Function
-    --' ', -- Constructor
-    --'', -- Field
-    --'[]', -- Variable
-    --'', -- Class
-    --'蘒', -- Interface
-    --'', -- Module
-    --'', -- Property
-    --'', -- Unit
-    --'', -- Value
-    --'', -- Enum
-    --'', -- Keyword
-    --'﬌', -- Snippet
-    --'', -- Color
-    --'', -- File
-    --'', -- Reference
-    --'', -- Folder
-    --'', -- EnumMember
-    --'', -- Constant
-    --'', -- Struct
-    --'', -- Event
-    --'ﬦ', -- Operator
-    --'<>', -- TypeParameter
-  --}
   
 end
 
