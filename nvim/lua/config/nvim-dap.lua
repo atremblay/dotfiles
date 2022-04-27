3e5eb8e5fc5eaf8d42b6dc119d323c6870280051
Alexis Tremblay <alexis.tremblay@gmail.com>
2022-04-27 08:26:00 -0400

    hack dap-python to have it use conda

--- nvim/lua/config/nvim-dap.lua
+++ nvim/lua/config/nvim-dap.lua
@@ -15,7 +15,7 @@ end
 
 dapui.setup()
 
-dap_python.setup(vim.g.python3_host_prog)
+--dap_python.setup(vim.g.python3_host_prog)
 dap_python.test_runner = "pytest"
 require("dap.ext.vscode").load_launchjs()
 
@@ -44,6 +44,7 @@ vim.keymap.set("n", "<F3>", "<cmd>lua require'dap'.step_into()<CR>", opts)
 vim.keymap.set("n", "<F4>", "<cmd>lua require'dap'.step_out()<CR>", opts)
 vim.keymap.set("n", "<F5>", "<cmd>lua require'dap'.terminate()<CR>", opts)
 vim.keymap.set("n", "<F6>", "<cmd>lua require'dap'.run_last()<CR>", opts)
+vim.keymap.set("n", "<F7>", "<cmd>lua require'dap'.run_to_cursor()<CR>", opts)
 vim.keymap.set("n", "<leader>b", "<cmd>lua require'dap'.toggle_breakpoint()<CR>", opts)
 vim.keymap.set("n", "<leader>sj", "<cmd>lua require'dap'.down()<CR>", opts) -- <s>tack <j>down
 vim.keymap.set("n", "<leader>sk", "<cmd>lua require'dap'.up()<CR>", opts) -- <s>tack <k>up
@@ -72,13 +73,54 @@ dap.listeners.before.event_exited["dapui_config"] = function()
 	dapui.close()
 end
 
-local pyadapter = require("dap").adapters.python
-pyadapter.enrich_config = function(config, on_config)
+-- Hack to have dap use conda_env if it's active.
+-- There has to be a better solution than this
+local default_setup_opts = {
+	include_configs = true,
+	console = "integratedTerminal",
+	pythonPath = nil,
+}
+local get_python_path = function()
+	local conda_venv_path = os.getenv("CONDA_PREFIX")
+	local venv_path = os.getenv("VIRTUAL_ENV")
+	if venv_path then
+		return venv_path .. "/bin/python"
+	end
+	if conda_venv_path then
+		return conda_venv_path .. "/bin/python"
+	end
+	return nil
+end
+
+local enrich_config = function(config, on_config)
 	if not config.pythonPath and not config.python then
-		local conda_venv_path = os.getenv("CONDA_PREFIX")
-		if conda_venv_path then
-			config.pythonPath = conda_venv_path .. "/bin/python"
-			on_config(config)
-		end
+		config.pythonPath = get_python_path()
+	end
+	on_config(config)
+end
+local adapter_python_path = vim.fn.expand(vim.g.python3_host_prog)
+opts = vim.tbl_extend("keep", opts or {}, default_setup_opts)
+dap.adapters.python = function(cb, config)
+	if config.request == "attach" then
+		local port = (config.connect or config).port
+		cb({
+			type = "server",
+			port = assert(port, "`connect.port` is required for a python `attach` configuration"),
+			host = (config.connect or config).host or "127.0.0.1",
+			enrich_config = enrich_config,
+			options = {
+				source_filetype = "python",
+			},
+		})
+	else
+		cb({
+			type = "executable",
+			command = adapter_python_path,
+			args = { "-m", "debugpy.adapter" },
+			enrich_config = enrich_config,
+			options = {
+				source_filetype = "python",
+			},
+		})
 	end
 end
