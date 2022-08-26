d823d78e01dfb1c16b56e6a09cfc4b241144983b
Alexis Tremblay <alexis.tremblay@gmail.com>
2022-08-26 15:54:47 -0400

    add snippets

--- nvim/.config/nvim/lua/config/snippets.lua
+++ nvim/.config/nvim/lua/config/snippets.lua
@@ -13,8 +13,46 @@ ls.add_snippets("markdown", {
             \begin{{{}}}
                 {}
             \end{{{}}}
-                ]],
+            ]],
 			{ i(1), i(0), rep(1) }
 		)
 	),
+	s(
+		"pmat",
+		fmt(
+			[[
+            \begin{{pmatrix}}
+                {}
+            \end{{pmatrix}}
+            ]],
+			{ i(0) }
+		)
+	),
+	s(
+		"bmat",
+		fmt(
+			[[
+            \begin{{bmatrix}}
+                {}
+            \end{{bmatrix}}
+            ]],
+			{ i(0) }
+		)
+	),
+	s("mk", fmt("${}$ {}", { i(1), i(0) })),
+	s(
+		"dm",
+		fmt(
+			[[
+            $$
+            {}
+            $$
+            ]],
+			{ i(0) }
+		)
+	),
+	s("//", fmt("\\frac{{{}}}{{{}}} {}", { i(1, "numerator"), i(2, "denominator"), i(0) })),
+	s("part", fmt("\\frac{{partial {}}}{{partial {}}} {}", { i(1, "numerator"), i(2, "denominator"), i(0) })),
+	s("lr(", fmt("\\left({}\\right) {}", { i(1), i(0) })),
+	s("lr{", fmt("\\left{{{}\\right}} {}", { i(1), i(0) })),
 })
