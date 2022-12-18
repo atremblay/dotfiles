46f4f1d8a43a8a1c0deea2cb86d0738fce8d4ae5
Alexis Tremblay <alexis@minus.local>
2022-12-18 15:34:57 -0500

    add remap

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -26,6 +26,11 @@ keymap("n", "Y", "y$", opts)
 keymap("n", "n", "nzzzv", opts)
 keymap("n", "N", "Nzzzv", opts)
 keymap("n", "J", "mzJ`z", opts)
+keymap("n", "<C-d>", "<C-d>zz", opts)
+keymap("n", "<C-u>", "<C-u>zz", opts)
+
+keymap("x", "<leader>p", [["_dP]])
+keymap("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 -- Add additional undo points
 keymap("i", ",", ",<c-g>u", opts)
@@ -34,8 +39,8 @@ keymap("i", "!", "!<c-g>u", opts)
 keymap("i", "?", "?<c-g>u", opts)
 
 -- Nice remap to move lines around
---keymap("v", "J", ":m '>+1<CR>gv=gv", opts)
---keymap("v", "K", ":m '<-2<CR>gv=gv", opts)
+keymap("v", "J", ":m '>+1<CR>gv=gv", opts)
+keymap("v", "K", ":m '<-2<CR>gv=gv", opts)
 --keymap("i", "<C-j>", "<esc>:m .+1<CR>==", opts)
 --keymap("i", "<C-k>", "<esc>:m .-2<CR>==", opts)
 --keymap("n", "<leader>j", ":m .+1<CR>==", opts)
@@ -71,7 +76,7 @@ vim.keymap.set("n", "<leader>rg", builtin.live_grep, opts)
 vim.keymap.set("n", "<C-b>", builtin.buffers, opts)
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, opts)
 vim.keymap.set("n", "<leader>ps", function()
-	builtin.grep_string({ search = vim.fn.input("rg > ") })
+    builtin.grep_string({ search = vim.fn.input("rg > ") })
 end, opts)
 
 --keymap("n", "<leader>f", "<cmd>lua require('telescope.builtin').find_files()<CR>", opts)
