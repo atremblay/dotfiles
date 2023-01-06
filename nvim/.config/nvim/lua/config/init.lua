69282a4ff73abf6ba6fedffe91ed78691f6d6024
Alexis Tremblay <alexis.tremblay@gmail.com>
2023-01-06 11:48:46 -0500

    fix order of init in nvim

--- nvim/.config/nvim/lua/config/init.lua
+++ nvim/.config/nvim/lua/config/init.lua
@@ -1,6 +1,6 @@
+require("config.plugins")
 require("config.options")
 require("config.keymaps")
-require("config.plugins")
 require("config.colorscheme")
 require("config.lsp")
 require("config.snippets")
