da420c75a7ed57a7c9b26146ef00930d9b49d05b
Alexis Tremblay <alexis@Mac-Studio.local>
2024-06-11 21:32:24 -0400

    update lazyvim

---
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,16 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  dependencies = {
+    "s1n7ax/nvim-window-picker",
+    config = function()
+      require("window-picker").setup()
+    end,
+  },
+  opts = {
+    window = {
+      mappings = {
+        ["l"] = "open",
+      },
+    },
+  },
+}
