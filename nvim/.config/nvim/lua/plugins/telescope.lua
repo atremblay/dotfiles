48e8c98254a7fc708cb3e78f46a308b988ee0d3a
Alexis Tremblay <atremblay@explorance.com>
2025-10-28 07:20:49 -0400

    nvim update

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -2,20 +2,32 @@ local Util = require("lazyvim.util")
 return {
     "nvim-telescope/telescope.nvim",
     cmd = "Telescope",
-    version = false, -- telescope did only one release, so use HEAD for now
+    version = false, 
     dependencies = {
-        {
-            "nvim-telescope/telescope-fzf-native.nvim",
-            build = "make",
-            enabled = vim.fn.executable("make") == 1,
-            config = function()
-                Util.on_load("telescope.nvim", function()
-                    require("telescope").load_extension("fzf")
-                end)
-            end,
-        },
+    {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = (build_cmd ~= "cmake") and "make"
+        or "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+      enabled = build_cmd ~= nil,
+      config = function(plugin)
+        LazyVim.on_load("telescope.nvim", function()
+          local ok, err = pcall(require("telescope").load_extension, "fzf")
+          if not ok then
+            local lib = plugin.dir .. "/build/libfzf." .. (LazyVim.is_win() and "dll" or "so")
+            if not vim.uv.fs_stat(lib) then
+              LazyVim.warn("`telescope-fzf-native.nvim` not built. Rebuilding...")
+              require("lazy").build({ plugins = { plugin }, show = false }):wait(function()
+                LazyVim.info("Rebuilding `telescope-fzf-native.nvim` done.\nPlease restart Neovim.")
+              end)
+            else
+              LazyVim.error("Failed to load `telescope-fzf-native.nvim`:\n" .. err)
+            end
+          end
+        end)
+      end,
     },
-    keys = {
+  },
+  keys= {
         {
             "<leader>,",
             "<cmd>Telescope buffers sort_mru=true sort_lastused=true<cr>",
