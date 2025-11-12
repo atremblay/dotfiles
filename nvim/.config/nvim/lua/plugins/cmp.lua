c9125500d3f227caf12f13e1341762e86e084c0f
Alexis Tremblay <alexis.tremblay@gmail.com>
2025-11-12 15:55:00 -0500

    Trying to change LSP config a little bit

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -1,6 +1,20 @@
 return {
     {
         "hrsh7th/nvim-cmp",
+        dependencies = {
+            "hrsh7th/cmp-buffer", -- source for text in buffer
+            "hrsh7th/cmp-path", -- source for file system paths
+            {
+                "L3MON4D3/LuaSnip",
+                -- follow latest release.
+                version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+                -- install jsregexp (optional!).
+                build = "make install_jsregexp",
+            },
+            "saadparwaiz1/cmp_luasnip", -- for autocompletion
+            "rafamadriz/friendly-snippets", -- useful snippets
+            "onsails/lspkind.nvim", -- vs-code like pictograms
+        },
         enable = false,
         opts = function()
             local cmp_status_ok, cmp = pcall(require, "cmp")
